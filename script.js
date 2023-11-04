0e2a9348c764e31196ac346f319f6e66764725a4
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2023-11-04 12:30:53 +0000

    it has started

---
+++ script.js
@@ -0,0 +1,12 @@
+ let hamburgerMenu = document.getElementById("hamburgerMenu");
+
+ let navbar = document.getElementById("navbar");
+
+
+
+ hamburgerMenu.addEventListener("click", function(){
+    hamburgerMenu.style.background ="red";
+    navbar.classList.add("active") ;
+
+ })
+
